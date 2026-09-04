2979db5d38512d5dd498aaac4e18f1a77ccde7f2
PakSpotlight <pakifun3@gmail.com>
2026-09-04 14:34:39 +0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/admin" || url.pathname === "/admin/") {
+      const asset = await env.ASSETS.fetch(new Request(new URL("/index.html", request.url), request));
+      const headers = new Headers(asset.headers);
+      headers.set("X-Robots-Tag", "noindex, nofollow, noarchive");
+      return new Response(asset.body, {status: asset.status, statusText: asset.statusText, headers});
+    }
     return env.ASSETS.fetch(request);
   }
 };
